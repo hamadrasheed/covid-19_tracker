55baf5c2f3f24c5b0971d4aa958d748f5e81956f
Hamad <50006928+hamadrasheed@users.noreply.github.com>
2020-04-14 19:25:40 +0500

    Add files via upload

---
+++ build/precache-manifest.2fd7327c4c561b644a74e1670a5791be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b811bb1fd20e1d1d10ae5a6dd6d160f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c589d5e9f3672bc14a81",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "5ddc79d9c1a3911e0b5e",
+    "url": "/static/css/main.8ec0f30e.chunk.css"
+  },
+  {
+    "revision": "c589d5e9f3672bc14a81",
+    "url": "/static/js/2.d1850517.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.d1850517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddc79d9c1a3911e0b5e",
+    "url": "/static/js/main.e6709b89.chunk.js"
+  },
+  {
+    "revision": "f175251ec165c8b54a1c",
+    "url": "/static/js/runtime-main.e52bfea7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
